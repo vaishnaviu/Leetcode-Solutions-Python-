c66772a8d21892d52556a3668505158eec5430d4
Vaishnavi Upadhye <vaishnaviu96@gmail.com>
2023-10-04 00:51:36 -0700

    Update Besttimetobuyandsell.py

--- EasyLevel/121. Best Time to Buy and Sell Stock/Besttimetobuyandsell.py
+++ EasyLevel/121. Best Time to Buy and Sell Stock/Besttimetobuyandsell.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        least = prices[0]
+        leastprice = prices[0]
         n = len(prices)
         maxprofit = 0
         for i in range(1,n):
-            if prices[i] < least:
-                least = prices[i]
-            elif prices[i]-least > maxprofit:
-                maxprofit = prices[i]-least        
+            if prices[i] < leastprice:
+                leastprice = prices[i]
+            elif prices[i]-leastprice > maxprofit:
+                maxprofit = prices[i]-leastprice        
         return maxprofit
